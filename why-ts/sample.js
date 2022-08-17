7d1e255de4b72d987ed161de278cb9bc48519f59
sangho235 <sangho235@it.gsshop.com>
2022-08-17 15:00:26 +0900

    1장 실습 마무리

---
+++ why-ts/sample.js
@@ -0,0 +1,17 @@
+/* function sum(a,b) {
+    return a + b;
+} */
+
+//@ts-check
+
+/**
+ * 
+ * @param {number} a  첫번쨰 숫자
+ * @param {number} b  두번쨰 숫자
+ * @returns 
+ */
+function sum(a,b) {
+    return a + b;
+}
+
+sum(10,'20'); // 오류를 찾아 주지 않음 // @ts-check에 의해 오류 체크 됨 
