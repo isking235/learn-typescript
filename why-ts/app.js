ac566ed4bc714de1873fbf7df24c96d7022a108c
sangho235 <sangho235@it.gsshop.com>
2022-08-17 14:34:57 +0900

    1장 타입스크립트 소개와 배경_자바스크립트에 타입이 있을 떄의 첫 번쨰 장점

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,45 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Adress
+ * @property {string} street
+ * @property {string} city
+*/
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Adress} address
+ */
+
+/**
+ * js doc의기본 문법
+ * 
+ * @returns {Promise<User>}</User>} 
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function(response) {
+  response.address
+});
+
+
 function startApp() {
-  axios
-    .get(url)
+  /* axios
+    .get(url) */
+    fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerHTML = user[0].username;
+      email.innerHTML = user[0].email;
+      address.innerHTML = user[0].address.street;
     })
     .catch(function (error) {
       console.log(error);
