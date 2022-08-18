2d6704d43799252d4570b2c4dbf378d0c4784e76
sangho235 <sangho235@it.gsshop.com>
2022-08-18 13:52:53 +0900

    2장 타입스크립트 기초

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+//js
+//var str = 'hello';
+
+//ts 문자열
+let str: string = 'hello';
+
+// 숫자
+let num : number = 10;
+
+// 배열
+let arr:Array<number> = [1,2,3];
+let heroes:Array<string> = ['Cat','Thor','Hulk', 10];
+let items:number[] = [1,2,3];
+
+
+//튜플
+let address : [string,number] = ['gangnam', 10];
+
+//객체
+let obj: object = {};
+let person2: object = {
+    name : 'capt',
+    age :100
+};
+
+let person :{name : string, age :number} = {
+    name : 'thor',
+    age : 1000
+}
+
+//진위값
+let show :boolean  = false;
---
+++ class-note/function.js
@@ -0,0 +1,5 @@
+function sum2(a, b) {
+    return a + b;
+}
+
+sum2(10, 20, 30,40,50); //이런경우 ts는 어떻꼐 처리 뙬까?
