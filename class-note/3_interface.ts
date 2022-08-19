5d4faf538716f3b13738c5118b9620b35274bada
sangho235 <sangho235@it.gsshop.com>
2022-08-19 16:43:09 +0900

    4장 인터페이스 딕셔너리 패턴

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -39,4 +39,21 @@ interface StringArray {
 }
 
 var arr : StringArray = ['a','b','c'];
-arr[0] = 10
+//arr[0] = 10
+//딕셔너리 패턴
+interface StringRegexDictionary {
+    [key:string] : RegExp;
+
+
+}
+
+var obj :StringRegexDictionary= {
+    //sth:/abc/,
+    cssFile :/\.css$/,
+    jsFILE : /\.js$/,
+}
+//obj['cssFile'] = 'a' 오류 발생 
+
+Object.keys(obj).forEach(function(value)) {
+
+}
