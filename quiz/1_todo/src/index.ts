e9bb62e600b5bb282de8c069d96dad3520e6d5e1
sangho235 <sangho235@it.gsshop.com>
2022-08-19 15:23:15 +0900

    실습풀이3-함수구현

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -31,17 +31,33 @@ function completeTodo(index: number, todo:object) :void {
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo() :object{
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted() : object[] {
   return todoItems.filter(item => item.done);
+  /* return todoItems.filter(function(item) {
+    if(item.done) {
+      return item;
+    }
+  }); */
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems() :void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const item1 = {
+    id: 4,
+    title: '아이템4',
+    done : false,
+  };
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title : '아이템 5',
+    done : false,
+  });
 }
 
 // NOTE: 유틸 함수
