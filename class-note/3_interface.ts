1aede2967e1fd0789d5abaa90149d73efc3e1526
sangho235 <sangho235@it.gsshop.com>
2022-08-19 16:06:37 +0900

    4장_함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -4,7 +4,20 @@ interface User {
 
 }
 
+//변수에 활용한 인터페이스 활용
 var seho : User = {
     age : 33,
     name : '세호'
 }
+
+//함수에 인터페이스 활용
+function getUser(user:User) {
+    console.log(user);
+
+}
+
+const capt = {
+    name : '캡틴',
+    age : 100
+}
+getUser(capt);
