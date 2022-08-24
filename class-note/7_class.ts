c1ff7ad297f06e5711093fbaed393df71be7f57e
sangho235 <sangho235@it.gsshop.com>
2022-08-24 09:22:13 +0900

    8장 클래스

---
+++ class-note/7_class.ts
@@ -0,0 +1,31 @@
+class Person {
+    private name : string;
+    public age : number;
+    readonly log : string;
+
+    constructor(name : string, age:number ) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
+
+//react hook
+//리액트 예전 문법 - 클래스 기반 코드
+class App extends React.Component {
+
+}
+
+//리액트 최신 문법 - 훅 기반의 함수형 코드
+function App() {
+    return <div>Hello World</div>
+}
+
+//VUE도 클래스를 볼일이 없음
+new Vue({
+    el : '',
+    setup() {
+
+    }
+});
