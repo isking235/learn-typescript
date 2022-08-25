297bf7e1de7bace8fd1293fae4a022632b02662b
sangho235 <sangho235@it.gsshop.com>
2022-08-25 12:58:20 +0900

    9장 제네릭의 타입 제한, 정의된 타입으로 타입을 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -68,3 +68,28 @@ interface Dropdown<T> {
 
 const obj1 : Dropdown<number> = {value:'abc', selected:false};//오류남
 const obj2 : Dropdown<string> = {value:'abc', selected:false}; //'abc' 가 string라 오류 안 남
+
+//제네릭의 타입 제한
+/* function logTextLength<T>(text:T[]): T[] {
+    console.log(text.length);
+    text.forEach(function (text) {
+        console.log(text);
+    })
+    return text;
+}
+
+logTextLength<string>(['hi','abc']);
+ */
+//제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length : number;
+}
+
+function logTextLength<T extends LengthType>(text:T):T{
+    text.length;
+    return text;
+}
+
+logTextLength('a');
+logTextLength(10);
+logTextLength({length :10});
